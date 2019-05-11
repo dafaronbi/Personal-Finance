ff7f29a3e034f4de89ceb844914784c0859c5e2e
Daniel Faronbi <danielfaronbi@gmail.com>
2019-05-11 01:11:32 -0500

    image name change

--- app/src/main/java/com/example/danielfaronbi/personalfinance/MainActivity.kt
+++ app/src/main/java/com/example/danielfaronbi/personalfinance/MainActivity.kt
@@ -60,16 +60,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_home -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_income -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_expense -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_history -> {
 
             }
         }
